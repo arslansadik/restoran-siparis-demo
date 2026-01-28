8dd5efb5625ea0168f7641c0f50bd16e4ea79094
arslansadik <arslansadik@gmail.com>
2026-01-28 21:47:14 +0300

    baxse64 fixed version

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-// demo js
+fetch('menu.json')
+.then(r=>r.json())
+.then(data=>{
+ document.getElementById('title').innerText=data.restaurantName;
+ const menu=document.getElementById('menu');
+ data.items.forEach(it=>{
+   const c=document.createElement('div');
+   c.className='card';
+   c.innerHTML=`
+     <img src="${it.image}">
+     <h3>${it.name}</h3>
+     <p>${it.desc}</p>
+     <strong>${it.price} ${data.currency}</strong>
+   `;
+   menu.appendChild(c);
+ });
+});
